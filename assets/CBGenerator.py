005e3fad63df5e060c82462f295409debd41dd7f
milonpabis <miloszpabis@onet.pl>
2024-03-11 14:30:05 +0100

    High resolution downloader

--- assets/CBGenerator.py
+++ assets/CBGenerator.py
@@ -1,10 +1,22 @@
 import random as rd
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
 
 URL = "https://relatedwords.io/"
 
-MAIN_DIV = "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/span/div[1]/div[1]"
+MAIN_DIV_XPATH = "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/span/div[1]/div[1]"
+
+ANCHOR_XPATH = "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div[1]/a[1]"
+ANCHOR_CLASS = "FRuiCf islib nfEiy"
+
+IMG_CLASS = ".sFlh5c.pT0Scc.iPVvYb"
+
+COOKIES_XPATH = "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/span"
 
 
 class CBGenerator:
@@ -35,17 +47,44 @@ class HQImageScraper:
     def __init__(self):
         self.url_queue = []
         self.image_urls = []
+        self.driver = webdriver.Chrome()
 
     def get_urls(self, word):
-        response = requests.get("https://www.google.com/search?q=" + word + "&tbm=isch")
-        try:
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            print("Error - url Scraper")
-            return
-        result = BeautifulSoup(response.text, "html.parser")
-        self.url_queue = [a["href"] for a in result.find_all("a") if a["href"].startswith("https://www.google.com/imgres?")]
-        print(self.url_queue)
+        self.driver.get("https://www.google.com/search?q=" + word + "&tbm=isch")
+        cookies = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, COOKIES_XPATH)))
+        cookies.click()
+
+        container = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, MAIN_DIV_XPATH)))
+        urls = container.find_elements(By.TAG_NAME, "img")
+        print("url", urls)
+        cc = 0
+        while not len(self.image_urls) == 10:
+            url = urls[cc]
+            url.click()
+            try:
+                img = WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, IMG_CLASS)))
+                if img.get_attribute("src") not in self.image_urls:
+                    self.image_urls.append(img.get_attribute("src"))
+            except Exception:
+                pass
+            cc += 2
+        self.driver.quit()
+
+
+        print(self.image_urls)
+
+
+    def download_images(self, word):
+        self.get_urls(word)
+
+        for url in self.image_urls:
+            response = requests.get(url)
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                print(e)
+            with open("output/" + str(rd.randint(1, 999999999999)) + ".jpg", "wb") as file:
+                file.write(response.content)
 
         
 
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print(generator.get_words())
 
     scraper = HQImageScraper()
-    scraper.get_urls("test")
+    scraper.download_images("dog")
